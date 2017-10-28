34d48d5da375705a72809a89744732d197eb0a60
iamchingel <iamsanketray123@gmail.com>
2017-10-29 01:24:41 +0530

    Works

--- OpenGoogleMaps/ViewController.swift
+++ OpenGoogleMaps/ViewController.swift
@@ -13,13 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func openGoogleMaps(_ sender: Any) {
+// Helps if google maps is installed on iphone
+        if (UIApplication.shared.canOpenURL(URL(string:"https://maps.google.com")!)) {
+            UIApplication.shared.open(URL(string:"https://maps.google.com?saddr=20.323658,85.814780&daddr=20.253541,85.819847&directionsmode=driving")!)
+        }
+        else {
+            print("Can't use comgooglemaps://");
+        }
+    }
 
 }
-
